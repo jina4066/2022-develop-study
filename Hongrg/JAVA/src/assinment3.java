e8f856ce17829244f584be1ebe4c1c9e3d1e3e25
Hongrg <chrish1616@gmail.com>
2022-07-26 18:44:46 +0900

    JAVA1

---
+++ Hongrg/JAVA/src/assinment3.java
@@ -0,0 +1,9 @@
+public class assinment3 {
+  /**
+   * @param args
+   */
+  public static void main ( String[] args) throws Exception {
+    System.out.println("Java Programming Lecture 2") ;
+
+  }
+}
