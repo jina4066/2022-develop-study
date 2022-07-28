dfa5eaafaa58f32bbf556106051e29467276bc79
Hongrg <chrish1616@gmail.com>
2022-07-28 17:24:40 +0900

    JAVA1

---
+++ Hongrg/JAVA/Day1/Assignment3.java
@@ -0,0 +1,8 @@
+public class Assignment3 {
+  public static void main (String[] args) {
+    double fah = 77.0 ;
+    double cel ;
+    cel = ((fah-32.0)/1.8);
+    System.out.println("화씨 "+ fah + " 도는 섭씨로 " + cel + "입니다.");
+  }
+}
--- Hongrg/JAVA/src/assinment3.java
+++
@@ -1,9 +0,0 @@
-public class assinment3 {
-  /**
-   * @param args
-   */
-  public static void main ( String[] args) throws Exception {
-    System.out.println("Java Programming Lecture 2") ;
-
-  }
-}
