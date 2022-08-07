dcfaee3833215e8a4956205f99b423927c52005d
kimjina <jina4066@naver.com>
2022-08-07 21:55:35 +0900

    assignment6

---
+++ kim jina/Java/findFactorial.java
@@ -0,0 +1,26 @@
+public class findFactorial {
+    public static void main(String[] args) throws Exception {
+        int num1 = 10, num2 = 4, num3 = 10;
+
+        System.out.printf("factorial(%d) = %d\n", num1, factorial(num1));
+        System.out.printf("factorial(%d, %d) = %d", num2, num3, factorial(num2, num3));
+    }
+    public static int factorial(int n) {
+        int a = 1;
+        int b = 1;
+        while (a <= n) {
+            b *= a;
+            a++;
+        }
+        return b; 
+    }
+    public static int factorial(int n1, int n2) {
+        int a = n1;
+        int b = n2;
+        while (a <= n2) {
+            b *= a;
+            a++;
+        }
+        return b;
+    }
+}
