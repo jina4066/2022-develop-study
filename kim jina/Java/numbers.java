3b05fb94fd01d8cf1ec0e85b12afd1de7f54ba3d
kimjina <jina4066@naver.com>
2022-08-12 23:23:19 +0900

    assignment7

---
+++ kim jina/Java/numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class numbers {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+        
+        for (int i = 0; i < 5; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(numbers[i]);
+            System.out.print(" ");
+        } 
+        sc.close();
+        
+    }
+}
