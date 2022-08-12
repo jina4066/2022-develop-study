73f5cd903db7c0dd33219efc071c308c9bf706fa
kimjina <jina4066@naver.com>
2022-08-13 00:22:00 +0900

    assignment

--- kim jina/Java/Array_4X4.java
+++ kim jina/Java/Array_4X4.java
@@ -4,7 +4,7 @@ public class Array_4X4{
       
       int num = 1;
       for (int row = 0; row < arr.length; row++) {
-        for (int col = 0; col < arr[row].length; col++) {
+        for (int col = 0; col < arr.length; col++) {
             arr[row][col] = num++;
             System.out.printf("%2d", arr[row][col]);
         }
